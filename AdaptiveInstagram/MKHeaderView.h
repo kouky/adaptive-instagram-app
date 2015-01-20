3c41747fe56f094457eb7e2d91ff6f70eb84aabc
Michael Koukoullis <mike@kouky.org>
2015-01-20 21:50:21 +1100

    HeaderView class files

---
+++ AdaptiveInstagram/MKHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  MKHeaderView.h
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 20/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKHeaderView : UIView
+
+@end
---
+++ AdaptiveInstagram/MKHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  MKHeaderView.m
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 20/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import "MKHeaderView.h"
+
+@implementation MKHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
