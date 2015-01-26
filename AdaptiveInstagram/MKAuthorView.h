064bc8a61e85fcd211088e10b3c11e91026292a5
Michael Koukoullis <mike@kouky.org>
2015-01-26 15:26:10 +1100

    AuthorView class files

---
+++ AdaptiveInstagram/MKAuthorView.h
@@ -0,0 +1,13 @@
+//
+//  MKAuthorView.h
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 26/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKAuthorView : UIView
+
+@end
---
+++ AdaptiveInstagram/MKAuthorView.m
@@ -0,0 +1,21 @@
+//
+//  MKAuthorView.m
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 26/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import "MKAuthorView.h"
+
+@implementation MKAuthorView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
