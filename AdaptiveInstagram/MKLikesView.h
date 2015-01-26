7ab2740dd26f2e1c5ccdad7be2fc531bb9297a75
Michael Koukoullis <mike@kouky.org>
2015-01-26 20:45:50 +1100

    Class files for MKLikesView

---
+++ AdaptiveInstagram/MKLikesView.h
@@ -0,0 +1,13 @@
+//
+//  MKLikesView.h
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 26/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKLikesView : UIView
+
+@end
---
+++ AdaptiveInstagram/MKLikesView.m
@@ -0,0 +1,13 @@
+//
+//  MKLikesView.m
+//  AdaptiveInstagram
+//
+//  Created by Michael Koukoullis on 26/01/2015.
+//  Copyright (c) 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import "MKLikesView.h"
+
+@implementation MKLikesView
+
+@end
